e7a8c51b26181c7e7e1d2b5b1db4475ef772274a
haiMingaa <146327681+haiMingaa@users.noreply.github.com>
2025-10-16 20:34:58 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ async function translate(text， from， to, options) {
     let { apiKey， model = "hunyuan-turbos-latest" } = config;
     
     // 设置默认请求路径
-    const requestPath = "https://api.hunyuan.cloud.tencent.com/v1";
+    const requestPath = "https://api.hunyuan.cloud.tencent.com/v1/chat/completions";
     
     const headers = {
         'Content-Type': 'application/json',
